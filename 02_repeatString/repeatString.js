e4c3b82f850468e05fd9cec235e6f59dbbbe1df0
Artemisa Ruci <artemisa.ruci@gmail.com>
2024-04-14 14:54:57 +0200

    Solve Ex 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(myString, myNum) {
+    myString = 'hey';
+    for (let i = 0; i > 0; i++) {
+        myNum = i;
+        
+    }
 };
 
 // Do not edit below this line
