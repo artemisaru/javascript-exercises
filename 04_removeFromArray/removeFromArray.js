c070e2c8ef7b0b8475c5cc7f2736bb1762b5f0ba
Artemisa Ruci <artemisa.ruci@gmail.com>
2024-04-16 17:07:57 +0200

    Solve Ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...itemsToRemove) {
+    const filteredArr = myArray.filter(checkItems);
+    
+    function checkItems (item) {
+        return !itemsToRemove.includes(item);
+    }
+    
+    return filteredArr;
 };
+
+/*
+    Filter through the array with a condition (creates a shallow copy of the array with the elements that pass the test a.k.a condition)
+    True - includes the element
+    False - excludes the element
+*/
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
