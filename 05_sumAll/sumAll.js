c7188d4ec0274f5523f019de7d402fb359d1cc6e
Artemisa Ruci <artemisa.ruci@gmail.com>
2024-04-16 18:40:40 +0200

    Solve Ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
+    let start = 0;
+    let end = 0;
 
-};
-
+    if (numOne < 0 || numTwo < 0 || typeof(numOne) !== 'number' || typeof(numTwo) !== 'number') {
+        return 'ERROR';
+    } else if (numOne < numTwo) {
+        start = numOne - 1;
+        end = numTwo;
+    } else if (numOne > numTwo) {
+        start = numTwo - 1;
+        end = numOne;
+    }
+    
+    while (start <= end) {
+        sum += start;
+        start++;
+    }
+    
+    return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
